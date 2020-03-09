20e3804e0e929b5049682347294e2ce7136b79a5
Lasse Diercks <github@lassediercks.de>
2020-03-09 20:44:46 +0100

    introduce vue and table per city

--- freelancers.js
+++ freelancers.js
@@ -1,28 +1,43 @@
-const freelancers = [
-  {
-    name: "Linus Rogge",
-    title: "FE Dev / UI/UX Design",
-    website: "https://linuscodes.com",
-    mail: "moin@linuscodes.com",
-    city: "Hamburg"
+const data = {
+  hamburg: {
+    name: "hamburg",
+    freelancer: [
+      {
+        name: "Linus Rogge",
+        title: "FE Dev / UI/UX Design",
+        website: "https://linuscodes.com",
+        mail: "moin@linuscodes.com"
+      },
+      {
+        name: "Karsten Buckstegge",
+        title: "Illustration & Development",
+        website: "https://karstenbuckstegge.de"
+      },
+      {
+        name: "Hannes Diercks",
+        title: "JavaScript architect",
+        website: "https://xiphe.ne"
+      }
+    ]
   },
-  {
-    name: "Karsten Buckstegge",
-    title: "Illustration & Development",
-    website: "https://karstenbuckstegge.de",
-    city: "Hamburg"
-  },
-  {
-    name: "Hannes Diercks",
-    title: "JavaScript architect",
-    website: "https://xiphe.ne",
-    city: "Hamburg"
-  },
-  {
-    name: "Justin Schueler",
-    title: "Digital Designer",
-    website: "https://jschueler.com",
-    mail: "hej@jschueler.com",
-    city: "Leipzig"
+  leipzig: {
+    name: "leipzig",
+    freelancer: [
+      {
+        name: "Justin Schueler",
+        title: "Digital Designer",
+        website: "https://jschueler.com",
+        mail: "hej@jschueler.com"
+      }
+    ]
   }
-];
+};
+
+var app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      data
+    };
+  }
+});
